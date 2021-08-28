e4bda7b8d89a2722e7d556a31ba8c19566e3f2a0
gsub2000 <gayatri.sub2000@gmail.com>
2021-08-28 15:47:08 -0700

    added Procfile

--- main.py
+++ main.py
@@ -4,8 +4,7 @@ from flask_cors import CORS, cross_origin
 import json
 
 # list of email information
-currEmail = ("", False)
-
+currEmail = ["", False]
 # Creating Python Flask object (creates the server)
 app = Flask(__name__)
 cors = CORS(app)
@@ -14,23 +13,24 @@ app.config['CORS_HEADERS'] = "Content-Type"
 # Root function
 @app.route('/')
 def root():
-  return json.dumps("home page")
+	return json.dumps("home page")
 
 # TODO: Create a Page that sends the picture file
 @app.route('/test')
 def testImage():
-  ip_list = request.access_route
-  print(ip_list[0])
+    ip_list = request.access_route
+    print(ip_list[0])
+    
+    temp = ip_list[0].split('.')
+    my_ip = temp[0] + '.' + temp[1]
 
-  temp = ip_list[0].split('.')
-  my_ip = temp[0] + '.' + temp[1]
-
-  if my_ip == '130.211':
-    print("I opened the image!")
-  else:
-    currEmail[1] = True;
-	#my_list.append([email uid, status])
-  return send_file("smallpixel.png", mimetype="image/gif")
+    if my_ip == '130.211':
+        print("I opened the image!")
+        currEmail[1] = True
+    else:
+        currEmail[1] = True
+    
+    return send_file("smallpixel.png", mimetype = "image/gif")
 
 # checking current status of the email we are tracking (checks if seen) 
 @app.route('/check')
@@ -43,9 +43,13 @@ def checkStatus():
 # this is to set the email that is being tracked
 @app.route('/track', methods=["POST"])
 def trackEmail():
-	id = request.get_json(force=True)['id']
+	id = ""
+	try:
+		id = request.get_json(force=True)['id']
+	except:
+		id = "Unknown"
 	global currEmail
-	currEmail = (id, False)
+	currEmail = [id, False)]
 	ret = "Tracking email: " + str(id)
 	return json.dumps(ret)
 # Running/Starting the server
